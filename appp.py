de5f9a783277c1eace7fa9352c2bcc980cc7d039
Kanha Nahak <kanhanahak17@gmail.com>
2024-10-15 17:19:56 +0530

    b

--- appp.py
+++ appp.py
@@ -7,8 +7,9 @@ st.image('_img.jpeg')
 House_size=st.number_input("please enter size of the house")
 Bedrooms=st.number_input("please enter no of bedrooms")
 
-model=joblib.load("mul_lin_app.pkl")
+model1=joblib.load("mul_lin_app.pkl")
 
 if st.button('predict'):
     output=model.predict([[House_size,Bedrooms]])
     st.write(f"the Price of the house is {output[0]}")
+
