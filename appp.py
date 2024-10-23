8b818d7675291fd4f931505ede267ad94d93f45f
Kanha Nahak <kanhanahak17@gmail.com>
2024-10-23 17:01:29 +0530

    nnn

--- appp.py
+++ appp.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import joblib
 import sklearn
+import numpy
 st.title('Price Predictor')
 st.image('_img.jpeg')
 
 House_size=st.number_input("please enter size of the house")
 Bedrooms=st.number_input("please enter no of bedrooms")
 
-model1=joblib.load("mul_lin_app.pkl")
+model=joblib.load("mul_lin_app.pkl")
 
-if st.button('predict'):
-    output=model1.predict([[House_size,Bedrooms]])
+if st.button('predict')
+    features=np.array([[House_size,Bedrooms]])
+    output=model.predict([[House_size,Bedrooms]])
     st.write(f"the Price of the house is {output[0]}")
 
